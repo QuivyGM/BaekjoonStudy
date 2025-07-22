3e4c95f5e49d329c5710c77d84c4d78f5bb574bb
Quivy <minchan0902@gmail.com>
2025-07-22 17:02:03 +0900

    test

---
+++ Silver V/1312_Decimal/1312_decimal.cpp
@@ -0,0 +1,12 @@
+// c++ long double etc only supports until 10th number
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    long double A, B, N;
+
+    cin >> A >> B >> N;
+
+    A/=B;
+}
